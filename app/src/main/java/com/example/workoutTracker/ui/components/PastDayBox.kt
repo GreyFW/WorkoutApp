766726b2b05866feeca733b1b8b3b43a93427b16
GreyF <greysforgettea@gmail.com>
2026-02-28 17:37:45 +0700

    dimensions draft

--- app/src/main/java/com/example/workoutTracker/ui/components/PastDayBox.kt
+++ app/src/main/java/com/example/workoutTracker/ui/components/PastDayBox.kt
@@ -1,4 +1,5 @@
 package com.example.workoutTracker.ui.components
 
 class PastDayBox {
+
 }
--- app/src/main/java/com/example/workoutTracker/ui/theme/Dimens.kt
+++ app/src/main/java/com/example/workoutTracker/ui/theme/Dimens.kt
@@ -1,2 +1,16 @@
 package com.example.workoutTracker.ui.theme
 
+import androidx.compose.ui.unit.dp
+
+object AppDimens {
+    val paddingSmall = 8.dp
+    val paddingMedium = 16.dp
+    val paddingLarge = 24.dp
+    val paddingExtraLarge = 32.dp
+    val paddingHuge = 48.dp
+
+    val iconSizeStandard = 24.dp
+    val dayBoxSize = 36.dp
+    val dividerThicknessStandard = 1.dp
+    val dividerThicknessThick = 2.dp
+}
