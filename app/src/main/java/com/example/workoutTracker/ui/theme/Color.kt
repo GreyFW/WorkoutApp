b605a06e6ef9148ab874d172f3946899d2bfdde5
GreyF <greysforgettea@gmail.com>
2026-02-28 17:36:25 +0700

    colors added

--- app/src/main/java/com/example/workoutTracker/ui/theme/Color.kt
+++ app/src/main/java/com/example/workoutTracker/ui/theme/Color.kt
@@ -9,3 +9,24 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF121212)
+val White = Color(0xFFFFFFFF)
+
+val BlueBGLight = Color(0xFFE9EBF2)
+val BlueBGDark = Color(0xFFDFE5F2)
+
+val BlueField = Color(0xFFC0CCE6)
+
+val BlueAccent = Color(0xFF3113B6)
+val BlueAccentShadow = Color(0x66300D81)
+
+val BlueAccentSecondary = Color(0xFF5166CE)
+
+val TextBlueMain = BlueAccent
+val TextBlueSecondary = BlueAccentSecondary
+
+val AccentGreen = Color(0xFF4CAF50)
+val AccentOrange = Color(0xFFE65100)
+val TextGray = Color.LightGray
+val DividerGray = Color.LightGray
